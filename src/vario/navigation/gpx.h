1ff79f697314385b0414d2d18720a539bfdfacc8
oxothnk423 <oxothnk@gmail.com>
2026-08-31 12:00:16 -0700

    Use 400m default waypoint radius

--- src/vario/navigation/gpx.h
+++ src/vario/navigation/gpx.h
@@ -7,7 +7,7 @@
 #include "navigation/nav_ids.h"
 
 // Waypoint definition and memory allocation
-#define defaultWaypointRadius 150  // meters radius to count as "reaching/crossing" a waypoint
+#define defaultWaypointRadius 400  // meters radius to count as "reaching/crossing" a waypoint
 #define maxRoutes 10
 #define maxNavPoints 120
 #define maxRoutePointRefs 40
